c00a64ad1761e8764b2709a680e9ee0334bd28b4
Sebastian <sebastianlenart@gmail.com>
2023-01-30 21:37:08 +0000

    add UserContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./App";
+import { UserContextProvider } from "./context/UserContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<UserContextProvider>
+			<RouterProvider router={router} />
+		</UserContextProvider>
+	</React.StrictMode>
+);
