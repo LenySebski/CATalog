d94df3877b824fb1c5afeea6466b0c2316cf9132
Sebastian <sebastianlenart@gmail.com>
2023-02-01 02:19:29 +0000

    Styling update
    scss folder structure and minor changes in components to better match the figma design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import videoBg from "../assets/video-bg4.mp4";
+
 export const HomePage = () => {
-	return <div>Home Page</div>;
+	return (
+		<div className='layout__wrapper--home'>
+			<div className='layout__filler--home'>
+				<div className='overlay'></div>
+				<video src={videoBg} autoPlay loop muted />
+				<div className='overlay__text'>
+					<h1>Meow!</h1>
+				</div>
+			</div>
+			<div className='layout__content--home'>Hello</div>
+		</div>
+	);
 };
