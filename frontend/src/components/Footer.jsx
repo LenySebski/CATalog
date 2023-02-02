fe457f3d869ef54e34800b7e1c778cb9739991ac
Sebastian <sebastianlenart@gmail.com>
2023-02-02 22:51:38 +0000

    footer styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,22 +3,25 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-	const { user, setUser } = useContext(UserContext);
+	const { user } = useContext(UserContext);
 	return (
-		<footer className="footer__wrapper">
-			<div className="footer__column1">
+		<footer className='footer__wrapper'>
+			<div className='footer__column1'>
 				<img className='footer__logo-image' src={Logo}></img>
-				<p className="footer__description">CATalog is a website dedicated to helping cat owners find their lost pets.
-													Whether you are an owner looking for a beloved cat or just a cat lover who wants to help lost cats this site is for you!
-													</p>
+				<p className='footer__description'>
+					CATalog is a website dedicated to helping cat owners find their
+					lost pets. Whether you are an owner looking for a beloved cat or
+					just a cat lover who wants to help lost cats this site is for
+					you!
+				</p>
 			</div>
-			<div className="footer__column2">
-				<h4 className="footer__menu">Menu</h4>
+			<div className='footer__column2'>
+				<h4 className='footer__header'>Menu</h4>
 				<>
 					<Link to='/signup'>Sign Up</Link>
 					<Link to='/login'>Sign In</Link>
@@ -26,13 +29,15 @@ function Footer() {
 					{user?.user && <Link to='/newpost'>Register cat</Link>}
 				</>
 			</div>
-			<div className="footer__column3">
-				<h4 className="footer__contact">Contact Us</h4>
-				<a href="catalog@catalog.com" className="footer__email">catalog@catalog.com</a>
-				<a className="footer__social" href="www.facebook.com">
+			<div className='footer__column3'>
+				<h4 className='footer__header'>Contact Us</h4>
+				<a href='catalog@catalog.com' className='footer__email'>
+					catalog@catalog.com
+				</a>
+				<a className='footer__social' href='www.facebook.com'>
 					<FontAwesomeIcon icon={faFacebook} />
 				</a>
-				<a className="footer__social" href="www.instagram.com">
+				<a className='footer__social' href='www.instagram.com'>
 					<FontAwesomeIcon icon={faInstagram} />
 				</a>
 			</div>
