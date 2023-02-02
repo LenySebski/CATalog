8b2ab4898a15e48186f6d95dfe2c51aea5cb2910
Sebastian <sebastianlenart@gmail.com>
2023-02-02 02:57:10 +0000

    Added react query, Database page and basic styling for mini pet cards

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,18 @@ import { UserContextProvider } from "./context/UserContext";
 import "@fontsource/lato/900.css";
 import "@fontsource/lato/700.css";
 import "@fontsource/lato/400.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<UserContextProvider>
-			<RouterProvider router={router} />
-		</UserContextProvider>
+		<QueryClientProvider client={queryClient}>
+			<UserContextProvider>
+				<RouterProvider router={router} />
+			</UserContextProvider>
+			{import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+		</QueryClientProvider>
 	</React.StrictMode>
 );
